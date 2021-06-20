478610bfdbff1b04f1b442f093cdd632793f422e
Desire Kaleba <desirekaleba@gmail.com>
2021-06-20 16:32:36 +0300

    chore: Lint code

--- src/models/user.js
+++ src/models/user.js
@@ -32,11 +32,13 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// userSchema.virtual('id').get(() => this._id.toHexString());
+userSchema.virtual('id').get(function() {
+  return this._id.toHexString();
+});
 
-// userSchema.set('toJSON', {
-//   virtuals: true,
-// });
+userSchema.set('toJSON', {
+  virtuals: true,
+});
 
 const User = mongoose.model('User', userSchema);
 
